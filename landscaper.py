aaf21abf3885ee079eba00b50eec69524f6f5561
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-08 23:55:50 -0500

    Landscaper App: user can use scissors to cut grass

--- landscaper.py
+++ landscaper.py
@@ -1,7 +1,7 @@
 # tools array
 tools = [
     {'name': "teeth", "generate": 1, "price": 0},
-    {'name': "scissor", "generate": 1, "price": 5},
+    {'name': "scissor", "generate": 5, "price": 5},
     {'name': "teeth", "generate": 1, "price": 0}
 ]
 
