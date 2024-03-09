2ad5db58fc690b656d1a6f32e63700bba49c5f0d
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-09 00:00:07 -0500

    landscaper: user can hire a team

--- landscaper.py
+++ landscaper.py
@@ -3,6 +3,7 @@ tools = [
     {'name': "teeth", "generate": 1, "price": 0},
     {'name': "scissor", "generate": 5, "price": 5},
     {'name': "lawnmower", "generate": 50, "price": 25},
-    {'name': "battery-powered lawnmower", "generate": 100, "price": 250}
+    {'name': "battery-powered lawnmower", "generate": 100, "price": 250},
+    {'name': "students", "generate": 100, "price": 500}
 ]
 
