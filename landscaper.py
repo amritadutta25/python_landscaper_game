c4afc2031b71c989f4cc0fd941b984d261f7dd50
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-08 23:57:55 -0500

    landscaper: user can buy battery-powered lawnmower

--- landscaper.py
+++ landscaper.py
@@ -2,6 +2,7 @@
 tools = [
     {'name': "teeth", "generate": 1, "price": 0},
     {'name': "scissor", "generate": 5, "price": 5},
-    {'name': "lawnmower", "generate": 50, "price": 25}
+    {'name': "lawnmower", "generate": 50, "price": 25},
+    {'name': "battery-powered lawnmower", "generate": 50, "price": 250}
 ]
 
