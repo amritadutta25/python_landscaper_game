dbdbb3f470df554491257d03670736fb658ff5ee
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-09 00:00:32 -0500

    landscaper: user can use a team to cut grass

--- landscaper.py
+++ landscaper.py
@@ -4,6 +4,6 @@ tools = [
     {'name': "scissor", "generate": 5, "price": 5},
     {'name': "lawnmower", "generate": 50, "price": 25},
     {'name': "battery-powered lawnmower", "generate": 100, "price": 250},
-    {'name': "students", "generate": 100, "price": 500}
+    {'name': "students", "generate": 250, "price": 500}
 ]
 
