48285c498d63d858ad043a8fa282e1e90b4f6b6f
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-08 23:56:50 -0500

    landscaper: user can use push lawnmower to cut grass

--- landscaper.py
+++ landscaper.py
@@ -2,6 +2,6 @@
 tools = [
     {'name': "teeth", "generate": 1, "price": 0},
     {'name': "scissor", "generate": 5, "price": 5},
-    {'name': "lawnmower", "generate": 1, "price": 25}
+    {'name': "lawnmower", "generate": 50, "price": 25}
 ]
 
