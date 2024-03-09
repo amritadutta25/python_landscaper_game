320f459c84323d83ba7d3fe0993b86f04a831cae
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-08 23:54:45 -0500

    landscaper: user can use teeth to cut gras

---
+++ landscaper.py
@@ -0,0 +1,7 @@
+# tools array
+tools = [
+    {'name': "teeth", "generate": 1, "price": 0},
+    {'name': "scissor", "generate": 1, "price": 0},
+    {'name': "teeth", "generate": 1, "price": 0}
+]
+
